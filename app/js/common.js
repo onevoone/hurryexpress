68fdf4df5a6c7e1dbcfad58f2f694d57b9f99592
vladimir.fedorchenko <vladimir.softkit@casefair.dk>
2020-09-30 12:29:25 +0300

    finish main page. mock ajax.

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,25 @@
-$(function() {
+var $trackForm = $('#track-form');
 
-	// Custom JS
 
-});
+function makeRequst(data) {
+	$.ajax({
+		type: 'GET',
+		dataType: 'json',
+		url: '/response.json',
+		data : JSON.stringify(data),
+		success: function(data) {
+			alert(data.response);
+		}  
+	});
+}
+
+
+if ($trackForm.length > 0) {
+	$('form button[type="submit"]').bind('click', function (event) {
+		if (event) event.preventDefault()
+
+		var form_values = $trackForm.serializeArray();
+		makeRequst({ trackNumber: form_values[0].value });
+
+	})
+}
